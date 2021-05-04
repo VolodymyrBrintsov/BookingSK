bf4afb55edc998c4e12c736abcdf710275613bdd
VolodymyrBrintsov <brintsov@gmail.com>
2021-05-04 16:04:56 +0300

    some improvements in get_info

--- save_info.py
+++ save_info.py
@@ -10,17 +10,27 @@ link = 'https://portal.minv.sk/wps/portal/!ut/p/a1/' \
         'UmimoqIAwD3YeYAfo9DPYxlNUVJjdW7AwNkrHwC9fGLX_tLZNLKyXI' \
         'xg6Gf-G_TyRU4FVJWvtZIVg3m_f2yU5fH80ShhcBNtJ7HMASf-ywfAwP42p' \
         'H4SypijeuVJUkoo40TMUk2R0AHg8AKhDk8Q-qRgTURDgJPLqfKzRetadwfspzGT/dl5/d5/L2dBISEvZ0FBIS9nQSEh/'
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get(link)
 driver.find_element(By.ID, 'langSK').click()
 time.sleep(3)
 
 current_row = 0
+current_field = 0
 while True:
-        time.sleep(1)
         page_src = sp(driver.page_source, 'html.parser')
-        form = page_src.find_all('fieldset')[0]
-        row = form.find_all('div', 'row')[current_row]
+        form = page_src.find_all('fieldset')[current_field]
+        rows = form.find_all('div', 'row')
+        if current_row == len(rows):
+                driver.find_element(By.ID, 'submitter1').click()
+                current_row = 0
+                current_field += 1
+                time.sleep(5)
+                continue
+        row = rows[current_row]
+        if row.parent.get('class')[0] == 'hidden' or row.get('type') == 'hidden':
+                current_row+=1
+                continue
         label = row.find('div', 'key').get_text().strip()
         if '*' in label:
                 field = row.find('div', 'value').next_element.next_element
@@ -28,6 +38,14 @@ while True:
                 if field.name == 'select':
                         question = inquirer.List(id, message=label, choices=[option.text.strip() for option in field.find_all('option')]),
                         answer = inquirer.prompt(question)
-                        driver.find_element(By.XPATH, f'//{field.name}[@id="{id}"]/option[text()="{answer}"]').click()
-                        current_row+=1
-                        continue
+                        driver.find_element(By.XPATH, f'//{field.name}[@id="{id}"]/option[text()="{answer[id]}"]').click()
+                        current_row += 1
+
+                elif field.name == 'input':
+                        question = [inquirer.Text(id, message=label)]
+                        answer = inquirer.prompt(question)
+                        print(answer)
+                        driver.find_element(By.XPATH, f'//{field.name}[@id="{id}"]').send_keys(answer[id])
+                        current_row += 1
+        else:
+                current_row += 1
